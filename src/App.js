d8dcaa2d0bf6613f6ea508487e74e7a57de8a651
hayoun <fishingest@kookmin.ac.kr>
2022-08-03 03:44:06 +0900

    [FEAT] State Context, Dispatch Context

--- src/App.js
+++ src/App.js
@@ -8,25 +8,97 @@ import Diary from './pages/Diary';
 import New from './pages/New';
 import MyButton from './components/MyButton';
 import MyHeader from './components/MyHeader';
+import React, { useReducer, useRef } from 'react';
+
+const reducer=(state,action)=>{
+  let newState=[];
+  switch(action.type){
+  case 'INIT':{
+    return(action.data)
+  }
+  case 'CREATE':{
+    const newItem={
+      ...action.data
+    };
+    newState=[newItem,...state]
+    break;
+  }
+  case 'REMOVE':{
+    newState=state.filter((it)=>it.id!==action.targetId);
+    break;
+  }
+  case 'EDIT':{
+    newState=state.map((it)=>it.id === action.data.id ? {...action.data}: it)
+  }
+  default:
+    return state;
+  return newState;
+};
+}
+
+
+export const DiaryStaeteContext=React.createContext();
+export const DiaryDispatchContext=React.createContext();
 function App() {
+  
+  
+  const [data,dispatch]= useReducer(reducer,[1])
+
+
+
+  const dataId=useRef(0);
+
+  const onCreate=(date,content,emotion)=>{
+    dispatch({type:"CREATE",
+    data:{
+      od:dataId.current,
+      date:new Date(date).getTime(),
+      content,
+      emotion,
+    },
+  });
+  dataId.current +=1;
+  }
+
+  const onRemove=(targetId)=>{
+    dispatch({type:"REMOVE",targetId});
+  }
+
+  const onEdit = (targetId,date,content,emotion)=>{
+    dispatch({
+      type:"EDIT",
+      data:{
+        id:targetId,
+        date:new Date(date).getTime(),
+        content,
+        emotion
+
+      }
+    })
+  }
   return (
+    <DiaryStaeteContext.Provider value={data}>
+      <DiaryDispatchContext.Provider value={{onCreate,onEdit,onRemove}}>
     <BrowserRouter>
     <div className="App">
-      <MyHeader headText={"App"} leftChild={<MyButton text={"왼쪽 버튼"} onClick={()=>alert("dd")}/>}
-      rightChild={<MyButton text={"오른쪽 버튼"} onClick={()=>alert("dd")}/>}/>
+      {/* <MyHeader headText={"App"} leftChild={<MyButton text={"왼쪽 버튼"} onClick={()=>alert("dd")}/>} */}
+      {/* rightChild={<MyButton text={"오른쪽 버튼"} onClick={()=>alert("dd")}/>}/> */}
 
 
       
-      <h2>App.js</h2>
-      {/* <img src={process.env.PUBLIC_URL+`/assets/emotion1.png`}/>
-      <img src={process.env.PUBLIC_URL+`/assets/emotion2.png`}/>
+     
+      {/* <img src={process.env.PUBLIC_URL+`/assets/emotion1.png`}/> */}
+      {/* <img src={process.env.PUBLIC_URL+`/assets/emotion2.png`}/>
       <img src={process.env.PUBLIC_URL+`/assets/emotion3.png`}/>
       <img src={process.env.PUBLIC_URL+`/assets/emotion4.png`}/>
-      <img src={process.env.PUBLIC_URL+`/assets/emotion5.png`}/> */}
+      <img src={process.env.PUBLIC_URL+`/assets/emotion5.png`}/> */} 
       {/* process.env.PUBLIC_URL은 무조건 public을 가리키게됨 */}
-      <MyButton text={'버튼'} onClick={()=>alert('버튼 클릭')} type={'positive'}></MyButton>
+      {/* <MyButton text={'버튼'} onClick={()=>alert('버튼 클릭')} type={'positive'}></MyButton>
       <MyButton text={'버튼'} onClick={()=>alert('버튼 클릭')} type={'negative'}></MyButton>
-      <MyButton text={'버튼'} onClick={()=>alert('버튼 클릭')} ></MyButton>
+      <MyButton text={'버튼'} onClick={()=>alert('버튼 클릭')} ></MyButton> */}
+
+
+      
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/new' element={<New></New>}></Route>
@@ -37,6 +109,8 @@ function App() {
       
     </div>
     </BrowserRouter>
+    </DiaryDispatchContext.Provider>
+    </DiaryStaeteContext.Provider>
   );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,13 @@
+import { useContext } from "react";
+import { DiaryStaeteContext } from "../App";
+
 const Home=()=>{
+    const data=useContext(DiaryStaeteContext)
     return <div>
 
         <h1>Home</h1>
         <p>이곳은 홈입니다.</p>
+        {data[0]}
     </div>
 }
 
